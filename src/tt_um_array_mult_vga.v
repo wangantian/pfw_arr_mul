eeb9b1af56786038fee2fc96dd9a62b5e1ebea80
wangantian <atwangsh@gmail.com>
2026-05-05 09:59:46 -0400

    Update tt_um_array_mult_vga.v

--- src/tt_um_array_mult_vga.v
+++ src/tt_um_array_mult_vga.v
@@ -87,7 +87,7 @@ module tt_um_array_mult_vga (
   wire [3:0]  P_ones;//     = P_bcd[3:0];
 
 
-bin2bcd_8bit(.X(P_bcd),.Hundred(P_hundreds),.Tens(P_tens),.Ones(P_ones));
+bin2bcd_8bit bin2bcd_8bit_inst(.X(P_bcd),.Hundred(P_hundreds),.Tens(P_tens),.Ones(P_ones));
   // function [11:0] bin_to_bcd;
     // input [7:0] bin;
     // integer i;
