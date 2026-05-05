93e46298b26c1c0985774587675b341c551e1259
wangantian <atwangsh@gmail.com>
2026-05-05 13:45:19 -0400

    tiny mascot

--- src/tt_um_array_mult_vga.v
+++ src/tt_um_array_mult_vga.v
@@ -156,12 +156,13 @@ module tt_um_array_mult_vga (
   localparam MULT_START = CARD_Y1 + mult_gap;
   wire [7:0] pixel_color;
   
-  wire logo_pixels = ((hpos > CARD_X0 ) && (hpos <= CARD_X0 + 64)) &&( (vpos > MULT_START + 8*(bit_size+bit_gap) + bit_size) && (vpos <= 64 + MULT_START + 8*(bit_size+bit_gap) + bit_size));
+  wire logo_pixels = ((hpos > CARD_X0 ) && (hpos <= CARD_X0 + 32)) &&( (vpos > MULT_START + 8*(bit_size+bit_gap) + bit_size) && (vpos <= 32 + MULT_START + 8*(bit_size+bit_gap) + bit_size));
   wire [9:0] hpos_adj = hpos - CARD_X0;
   wire [9:0] vpos_adj = vpos -  (MULT_START + 8*(bit_size+bit_gap) + bit_size);
   
   //mastodon_rom   mastodon_rom_inst (.x(hpos_adj[6:0]), .y(vpos_adj[6:0]) ,.pixel(pixel_color)); 
-  mastodon_rom_mini   mastodon_rom_inst (.x(hpos_adj[5:0]), .y(vpos_adj[5:0]) ,.pixel(pixel_color)); 
+  //mastodon_rom_mini   mastodon_rom_inst (.x(hpos_adj[5:0]), .y(vpos_adj[5:0]) ,.pixel(pixel_color)); 
+  mastodon_rom_tiny   mastodon_rom_inst (.x(hpos_adj[4:0]), .y(vpos_adj[4:0]) ,.pixel(pixel_color)); 
 
   wire in_mul_array0 = in_mul_array_x_bound && (vpos >= MULT_START + 0*(bit_size+bit_gap)) && (vpos < MULT_START + 0*(bit_size+bit_gap) + bit_size);
   wire in_mul_array1 = in_mul_array_x_bound && (vpos >= MULT_START + 1*(bit_size+bit_gap)) && (vpos < MULT_START + 1*(bit_size+bit_gap) + bit_size);
